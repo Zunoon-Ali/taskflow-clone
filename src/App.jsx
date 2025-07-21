afddcdae148a10e4a1286cb9414a2dea09e8b272
Muhammad Zunoon Ali <zunnoon2006@gmail.com>
2025-07-21 13:25:27 +0500

    commit first

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+function App() {
+
+  return (
+    <>
+
+      <h1 className="text-3xl font-bold underline bg-red-300">Hello</h1>
+      <p className="text-3xl font-bold underline bg-gray-800">sdads</p>
+    </>
+  )
+}
+
+export default App
